8a0392ec1861caa57d03bc17a11b3476f42b1f75
netop://ウエハ <paul@webb.page>
2019-07-23 13:38:14 -0500

    Refactor

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 <h1 align="center">lbry.fund</h1>
 
 <div align="center">
-  <strong>The LBRY Community Fund Come Build Your Project on the LBRY Protocol</strong>
+  <strong>Build your project with the LBRY protocol</strong>
 </div>
 
 <div align="center">
   <img src="https://spee.ch/7/lbryfund.png" title="Screenshot of lbry.fund"/>
 </div>
 
+
+
 ## Prerequisites
-- WEB Browser eg chrome,firefox and etc
+- Git
+- Text editor
+- Web browser
 
 ## Installation
-- Clone (or fork) this repository: git clone https://github.com/lbryio/lbry.fund
-- on lbry.fund directories Open Index.html via web browser
+```bash
+git clone https://github.com/lbryio/lbry.fund
+```
+
+## Development
+Open `index.html` in your browser and text editor to make changes.
+
+## Usage
+Visit [LBRY.fund](https://lbry.fund) to see this repo in production.
 
 ## Contributing
 Contributions to this project are welcome, encouraged, and compensated.
@@ -24,9 +35,9 @@ For more details, see [CONTRIBUTING.md](CONTRIBUTING.md).
 This project is MIT licensed. For the full license, see [LICENSE](LICENSE).
 
 ## Security
-We take security seriously. Please contact [security@lbry.io](mailto:security@lbry.com) regarding any security issues.
+We take security seriously. Please contact [security@lbry.com](mailto:security@lbry.com) regarding any security issues.
 
 [Our PGP key is here](https://keybase.io/lbry/key.asc) if you need it.
 
 ## Contact
-The primary contact for this project is [@julie](https://github.com/jsigwart)(julie@lbry.com).
+The primary contact for this project is [Julie Sigwart](https://github.com/jsigwart) (julie+github@lbry.com).
--- js/main.js
+++ js/main.js
@@ -3,17 +3,19 @@ jQuery(document).ready(function($) {
   // Header fixed and Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-      $('#header').addClass('header-fixed');
+      $(".back-to-top").fadeIn("slow");
+      $("#header").addClass("header-fixed");
     } else {
-      $('.back-to-top').fadeOut('slow');
-      $('#header').removeClass('header-fixed');
+      $(".back-to-top").fadeOut("slow");
+      $("#header").removeClass("header-fixed");
     }
   });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
+
+  $(".back-to-top").click(() => {
+    $("html, body").animate({
       scrollTop: 0
-    }, 1500, 'easeInOutExpo');
+    }, 1500, "easeInOutExpo");
+
     return false;
   });
 
@@ -21,46 +23,49 @@ jQuery(document).ready(function($) {
   new WOW().init();
 
   // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
+  $(".nav-menu").superfish({
     animation: {
-      opacity: 'show'
+      opacity: "show"
     },
     speed: 400
   });
 
   // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({
-      id: 'mobile-nav'
+  if ($("#nav-menu-container").length) {
+    const $mobile_nav = $("#nav-menu-container").clone().prop({
+      id: "mobile-nav"
     });
-    $mobile_nav.find('> ul').attr({
-      'class': '',
-      'id': ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this).next().toggleClass('menu-item-active');
-      $(this).nextAll('ul').eq(0).slideToggle();
+    $mobile_nav.find("> ul").attr({
+      "class": "",
+      "id": ""
+    });
+
+    $("body").append($mobile_nav);
+    $("body").prepend("<button type='button' id='mobile-nav-toggle'><i class='fa fa-bars'></i></button>");
+    $("body").append("<div id='mobile-body-overly'></div>");
+    $("#mobile-nav").find(".menu-has-children").prepend("<i class='fa fa-chevron-down'></i>");
+
+    $(document).on("click", ".menu-has-children i", function(e) {
+      $(this).next().toggleClass("menu-item-active");
+      $(this).nextAll("ul").eq(0).slideToggle();
       $(this).toggleClass("fa-chevron-up fa-chevron-down");
     });
 
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-      $('#mobile-body-overly').toggle();
+    $(document).on("click", "#mobile-nav-toggle", function(e) {
+      $("body").toggleClass("mobile-nav-active");
+      $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+      $("#mobile-body-overly").toggle();
     });
 
     $(document).click(function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
+      const container = $("#mobile-nav, #mobile-nav-toggle");
+
       if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+          $("#mobile-body-overly").fadeOut();
         }
       }
     });
@@ -69,57 +74,56 @@ jQuery(document).ready(function($) {
   }
 
   // Smoth scroll on page hash links
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+  $(".nav-menu a, #mobile-nav a, .scrollto").on("click", function() {
+    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+      const target = $(this.hash);
+
       if (target.length) {
-        var top_space = 0;
+        let top_space = 0;
 
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
+        if ($("#header").length) {
+          top_space = $("#header").outerHeight();
 
-          if( ! $('#header').hasClass('header-fixed') ) {
+          if (!$("#header").hasClass("header-fixed")) {
             top_space = top_space - 20;
           }
         }
 
-        $('html, body').animate({
+        $("html, body").animate({
           scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
+        }, 1500, "easeInOutExpo");
 
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
+        if ($(this).parents(".nav-menu").length) {
+          $(".nav-menu .menu-active").removeClass("menu-active");
+          $(this).closest("li").addClass("menu-active");
         }
 
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+          $("#mobile-body-overly").fadeOut();
         }
+
         return false;
       }
     }
   });
 
   // Gallery - uses the magnific popup jQuery plugin
-  $('.gallery-popup').magnificPopup({
-    type: 'image',
-    removalDelay: 300,
-    mainClass: 'mfp-fade',
+  $(".gallery-popup").magnificPopup({
     gallery: {
       enabled: true
     },
+    mainClass: "mfp-fade",
+    removalDelay: 300,
+    type: "image",
     zoom: {
-      enabled: true,
       duration: 300,
-      easing: 'ease-in-out',
-      opener: function(openerElement) {
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-      }
+      easing: "ease-in-out",
+      enabled: true,
+      opener: openerElement => openerElement.is("img") ? openerElement : openerElement.find("img")
     }
   });
 
   // custom code
-
 });
